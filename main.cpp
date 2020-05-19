338eb3246637df14fc128fe29d3614e9ad0ef30c
Jakub <jakub.opatowski@gmail.com>
2020-05-19 21:39:48 +0200

    Added support for parsing json documents

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello new project!\n";
+    return 0;
+}
