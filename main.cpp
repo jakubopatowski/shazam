5db6e4ae9115d50d3ba8875a54f76fdc480625b3
Jakub Opatowski <jakub.opatowski@gmail.com>
2020-05-24 21:56:36 +0200

    Added json support using nlohmann/json parser library

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include"nlohmann/json.hpp"
+#include <iostream>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 using json = nlohmann::json;
@@ -7,5 +7,6 @@ using json = nlohmann::json;
 int main(int argc, char *argv[])
 {
     cout << "Hello new project!\n";
+    json s;
     return 0;
 }
