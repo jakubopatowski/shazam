5a5a9e7242d829221c7867035a4f483413bff7b8
Jakub Opatowski <jakub.opatowski@gmail.com>
2020-05-27 20:27:33 +0200

    Added Googletest support to the project with initial project setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <fstream>
 
 using namespace std;
 using json = nlohmann::json;
@@ -7,6 +8,9 @@ using json = nlohmann::json;
 int main(int argc, char *argv[])
 {
     cout << "Hello new project!\n";
+    std::ifstream i("./tests/test.json");
     json s;
+    i >> s;
+    
     return 0;
 }
